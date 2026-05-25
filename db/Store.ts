e30d11b48a59248add0f4253a7c28d9e123331cb
ncs01060 <ncs01045@gmail.com>
2026-05-25 14:37:25 +0900

    feat(db): add createTodo function to Store

--- db/Store.ts
+++ db/Store.ts
@@ -9,3 +9,20 @@ export async function loadTodo() {
     return todos;
   }
 }
+
+export async function createTodo(title: string) {
+  try {
+    const data = await AsyncStorage.getItem("todos");
+    const todos = data ? JSON.parse(data) : [];
+    todos.push({
+      id: Date.now(),
+
+      title: `${title}`,
+      isComplate: false,
+    });
+    await AsyncStorage.setItem("todos", JSON.stringify(todos));
+    return true;
+  } catch {
+    throw Error();
+  }
+}
